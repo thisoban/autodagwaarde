a7a5263df6e00308b8528dea7726618c6c650308
thisoban <thisoban_1995@hotmail.com>
2020-10-02 09:17:51 +0200

    Add project files.

---
+++ Auto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDagWaarde
+{
+    public class Auto
+    {
+        private int KilometerStand;
+        public BrandstofSoort Brandstof;
+        public int KmStand { get; private set; } = 1;
+
+        private readonly string Kenteken;
+        private readonly int DagWaarde { get;  return (500000 / kilometerstand) * (int)Brandstof)}
+        public Auto(string kenteken, BrandstofSoort brandstofSoort)
+        {
+            Kenteken = kenteken;
+            Brandstof = brandstofSoort;
+        }
+
+        public int RijdKilometers(int getal)
+        {
+            if (getal > 0)
+            {
+                return getal;
+            }
+
+            return getal;
+        }
+
+        
+    }
+
+        
+
+    
+}
---
+++ BrandstofSoort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDagWaarde
+{
+   public enum BrandstofSoort
+    {
+        benzine = 100,
+        diesel = 150,
+        lpg = 90,
+        elektrische = 150
+    }
+}
